1907adf4d2037e4ef3c09cc11d99664716959792
jirbthagoras <hansjabriel@gmail.com>
2024-10-05 13:29:49 +0700

    [+] Add label

--- belajar-dockerfile.sh
+++ belajar-dockerfile.sh
@@ -11,3 +11,9 @@ RUN cat "hello/hello-world.txt"
 
 # to see the build detail and set the build uncached
 docker build -t jirbthagoras/run:latest run --progress=plain --no-cache
+
+# to execute a certain command after the image is running in container
+CMD cat "hello/hello-world.txt"
+
+# to add metadata in image
+LABEL author="Jabriel Hans Talula"
